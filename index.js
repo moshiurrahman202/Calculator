e4fd282b17d02cba7274d69988a59c0ff1403ff9
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-03-29 02:56:23 +0600

    Mini Calculator is done

---
+++ index.js
@@ -0,0 +1,41 @@
+console.log("Select an option: \n1. Add \n2. Subtract \n3. Multiply \n4. Divide");
+
+var num1 = prompt("Enter First Number: ");
+var num2 = prompt("Enter Second Number: ");
+var option = prompt("Choose an operation: ");
+var result = null;
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+option = parseInt(option);
+
+var num1Con = isNaN(num1);
+var num2Con = isNaN(num2);
+var optionCon = isNaN(option);
+
+if(num1Con || num2Con || optionCon){
+    console.log("Invalid Input!");
+}else{
+    switch (option) {
+        case 1:
+            result = num1 + num2;
+            break;
+        case 2:
+            result = num1 - num2;
+            break;
+        case 3:
+            result = num1 * num2;
+            break;
+        case 4:
+            result = num1 / num2;
+            break;
+    
+        default:
+            break;
+    }
+    if(result == null){
+        console.log("No Result!");
+    }else{
+        console.log("Result: " + result);
+    }
+}
